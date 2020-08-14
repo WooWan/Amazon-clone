7200a4823dd7dde42236b31b1e94b265ec470858
WooWan <woohobi@gmail.com>
2020-08-14 17:24:07 +0900

    making nav_bar, homepage

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Home.css';
+function Home() {
+    return (
+        <div>
+            
+            <img 
+            className="home__image"
+            src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_Home_v2_en_US_1x._CB429090084_.jpg" 
+            alt="home-img"/>
+        </div>
+    )
+}
+
+export default Home
